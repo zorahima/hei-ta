3ee56f899fde1ba222a71f4147492d731021b60e
zorahima <zorahima10@gmail.com>
2018-05-19 12:14:13 +0700

    Change footer name known as himakomsi ivent invitation

--- application/views/foot_admin.php
+++ application/views/foot_admin.php
@@ -3,8 +3,7 @@
               <div class="pull-right hidden-xs">
                 <b>Version</b> 2.4.0
               </div>
-              <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-              reserved.
+              <strong>Copyright &copy; 2018 - <a href="https://adminlte.io">Himakomsi Event Invitation</a>.</strong> Template by AdminLTE
             </footer>
 
   <!-- Add the sidebar's background. This div must be placed
