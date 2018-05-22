b29ae59e8cc486f62ec71527f7a5a27314c3a9b7
zorahima <zorahima10@gmail.com>
2018-05-22 16:57:36 +0700

    UDAH BISA DONG LOGIN LOGOUT BYE

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -84,6 +84,11 @@
 		}
 		
 		public function logout() {
+			$this->session->unset_userdata('user_id');
+			$this->session->unset_userdata('email');
+
+			$this->session->unset_userdata('isLoggedIn');
+			
 			$this->session->sess_destroy();
 			
 			redirect('login');
--- application/views/head_admin.php
+++ application/views/head_admin.php
@@ -104,7 +104,7 @@
                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                   </div>
                   <div class="pull-right">
-                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                    <a href="<?php echo base_url('Login/logout') ?>" class="btn btn-default btn-flat">Sign out</a>
                   </div>
                 </li>
               </ul>
