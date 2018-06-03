966d8e1258ddb1bf3179105681567970d4697b5c
zorahima <zorahima10@gmail.com>
2018-06-03 12:47:48 +0700

    insert icon preview

--- application/views/all_confirmation.php
+++ application/views/all_confirmation.php
@@ -56,6 +56,8 @@ if ($user_data['type']=='pengurus') {
                    <td> <?php echo $u->banyak ?> </td>
                    <td class="text-center">
                   
+                  <a href="<?php echo base_url ('Invitation/update_present_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #00c0ef; border-color: #fff"><i class="fa   fa-eye"></i></a>
+                     
                      <a href="<?php echo base_url ('Invitation/update_present_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #4e9e02; border-color: #fff"><i class="fa  fa-check-circle"></i></a>
                      <a href="<?php echo base_url ('Invitation/update_absent_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #d41912; border-color: #fff"><i class="fa fa-remove"></i></a>
                    </td>
