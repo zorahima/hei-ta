fa8a20ad0bdf158b6eb948a9776a18d395ad2b45
zorahima <zorahima10@gmail.com>
2018-06-25 13:09:42 +0700

    tambah modals invitation cancelation

--- application/views/all_invitation.php
+++ application/views/all_invitation.php
@@ -13,99 +13,99 @@ if ($user_data['type']=='pengurus') {
 }
 ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-      </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+  </section>
 
-      <!-- Main content -->
-      <!--  Main content --> 
-      <section class="content">
-        <div class="row">
-          <div class="col-xs-12">
+  <!-- Main content -->
+  <!--  Main content --> 
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
 
-            <div class="box">
-              <div class="box-header">
-
-
-                <h3 class="box-title">Tabel Undangan </h3>
-                
-              </div> 
+        <div class="box">
+          <div class="box-header">
 
 
+            <h3 class="box-title">Tabel Undangan </h3>
 
-              <!-- /.box-header -->
-              <div class="box-body">
-               <!--  <?php var_dump($user); ?> -->
-               <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    
-                    <th>Invitation Name</th>
-                    <th>Invitation Desciption </th>
-                    <th>Invitation Time</th>
-                    <th>Invitation Loc</th>
-                    <th>Invitation Confirmation</th>
-                    <th> Operation </th>
+          </div> 
 
-                    
-                  </tr>
-                  <?php $i=1; foreach ($invitation as $u) {?>
-                  <tr>
-                   <td> <?php echo $i; ?> </td>
-                   <td> <?php echo $u->activity_name ?> </td>
-                   <td> <?php echo $u->activity_desc ?> </td>
-                   <td> <?php echo $u->activity_times ?> </td>
-                   <td> <?php echo $u->activity_loc ?> </td>
-                   <td> <?php echo $u->invitation_status ?> </td>
-                   
-                   <td class="text-center">
-                  
-                     <a href="<?php echo base_url ('Invitation/update_accept_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #4e9e02; border-color: #fff"><i class="fa  fa-check-circle"></i></a>
-                     <a href="<?php echo base_url ('Invitation/update_decline_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #d41912; border-color: #fff"><i class="fa fa-remove"></i></a>
-                   </td>
-                 </tr>
 
-                 <!--- modals -->
-                 <div class="modal fade" id="ubah-user">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span></button>
-                          <h4 class="modal-title"> Ubah data User</h4>
-                        </div>
-                        <div class="modal-body">
-                          <form method ="post" action="<?php echo base_url('index.php/Pengurus/tambah_data'); ?>" role="form">
-              
-          </form>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                      </div>
-                      <!-- /.modal-content -->
+
+          <!-- /.box-header -->
+          <div class="box-body">
+           <!--  <?php var_dump($user); ?> -->
+           <table id="example2" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>No</th>
+
+                <th>Invitation Name</th>
+                <th>Invitation Desciption </th>
+                <th>Invitation Time</th>
+                <th>Invitation Loc</th>
+                <th>Invitation Confirmation</th>
+                <th> Operation </th>
+
+
+              </tr>
+              <?php $i=1; foreach ($invitation as $u) {?>
+              <tr>
+               <td> <?php echo $i; ?> </td>
+               <td> <?php echo $u->activity_name ?> </td>
+               <td> <?php echo $u->activity_desc ?> </td>
+               <td> <?php echo $u->activity_times ?> </td>
+               <td> <?php echo $u->activity_loc ?> </td>
+               <td> <?php echo $u->invitation_status ?> </td>
+
+               <td class="text-center">
+
+                 <a href="<?php echo base_url ('Invitation/update_accept_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #4e9e02; border-color: #fff"><i class="fa  fa-check-circle"></i></a>
+                 <button type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#input_cancelation<?php echo $u->activity_id ?>" style="background:#1a75ff; border-color:#fff"><i class="fa fa-pencil"></i>
+                 </button>
+                 <a href="<?php echo base_url ('Invitation/update_decline_invitation/'.$u->invitation_id) ?> " class="btn btn-sm btn-info" style="background: #d41912; border-color: #fff"><i class="fa fa-remove"></i></a>
+               </td>
+             </tr>
+
+                          <!-- Modal -->
+             <div class="modal fade" id="input_cancelation<?php echo $u->activity_id ?>">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title"> Tambahkan Alasan Tidak Hadir </h4>
                     </div>
-                    <!-- /.modal-dialog -->
-                  </div>
-                  <!-- /.modal -->
-                  <?php $i++; }  ?>
-                </thead>
-                <tbody>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+                    <div class="modal-body">
+                      <form class="form-horizontal" method ="post"  id="formtambahalasan<?php echo $u->activity_id ?>" action="<?php echo base_url('Todo/updateTodo'); ?>" role="form">
+                        <input type="hidden" name='proker_id' id='proker_id'>
 
-        </section>
-        <!-- /.content -->
+                        <div class="box-body">
+                          <label class="col-sm-3 control-label">Alasan Tidak Hadir </label>
+                          <div class="col-sm-9">
+                          <textarea class="form-control" rows="3"  id="invitation_cancelation" name="invitation_cancelation" placeholder="Enter ..."></textarea>
+                          
+                            <input type="hidden" class="form-control" name="activity_id" value="<?php echo $u->activity_id ?>" required>
+                          </div>
+                        </div>
+
+              <?php $i++; }  ?>
+            </thead>
+            <tbody>
+            </tfoot>
+          </table>
+        </div>
+        <!-- /.box-body -->
       </div>
-      <!-- /.content-wrapper -->
-<?php
-$this->load->view('foot_admin');
-?>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php
+  $this->load->view('foot_admin');
+  ?>
